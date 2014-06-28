17e4f686e55f3507086e66af90bd33f47fd1124b
Dan Manocchio <vytis98@gmail.com>
2014-06-27 21:20:27 -0400

    initial checkin

---
+++ gitTest/src/com/madsolutions/gittest/GitTestApplication.java
@@ -0,0 +1,13 @@
+package com.madsolutions.gittest;
+
+public class GitTestApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
