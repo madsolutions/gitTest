3c2b486c399efca47dde4e80f4d48589010ecbde
Dan Manocchio <dan.manocchio@gmail.com>
2014-07-14 21:10:15 -0400

    commit 2 on Main branch

---
+++ gitTest/src/com/madsolutions/gittest/MainBranchFeature1.java
@@ -0,0 +1,13 @@
+package com.madsolutions.gittest;
+
+public class MainBranchFeature1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
