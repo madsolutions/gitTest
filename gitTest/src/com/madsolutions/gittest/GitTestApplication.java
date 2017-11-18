7c5f07c017543e83adeb4929259b92a4e88ede10
danman <dan.manocchio@gmail.com>
2017-11-18 17:52:57 -0500

    Simple change while playing with GitKraken

--- gitTest/src/com/madsolutions/gittest/GitTestApplication.java
+++ gitTest/src/com/madsolutions/gittest/GitTestApplication.java
@@ -7,7 +7,8 @@ public class GitTestApplication {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Hello again");
+		System.out.println("Hello a 2nd time");
+		System.out.println("Hello a 3rd time");
 
 	}
 
